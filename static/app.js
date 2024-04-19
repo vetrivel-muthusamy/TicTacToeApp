4b3eb2543acbbdc51072f397cfed54aa5b176c92
Muthusamy <vetrivel.muthusamy@capgemini.com>
2024-04-19 09:12:35 +0530

    Fix for the 3*3 Box appearnce in the GUI

--- static/app.js
+++ static/app.js
@@ -1,10 +1,19 @@
 // app.js
 
 $(document).ready(function() {
+    // Generate game board dynamically
+    var boardHtml = "";
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            boardHtml += "<div class='cell' data-row='" + i + "' data-col='" + j + "'></div>";
+        }
+    }
+    $("#board").append(boardHtml);
+
     // Add click event handler for cells
     $(".cell").click(function() {
-        var row = $(this).parent().index(); // Get the row index of the clicked cell
-        var col = $(this).index(); // Get the column index of the clicked cell
+        var row = $(this).data("row");
+        var col = $(this).data("col");
         alert("Clicked cell at row " + row + ", column " + col);
     });
 });
